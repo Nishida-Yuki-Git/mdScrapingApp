7d21387855990fd39bd93f43954ca7c4f544ee2b
Nishida-Yuki-Git <yellow.heart788@gmail.com>
2022-09-11 18:12:38 +0900

    クラスコメントとメソッドコメントを追加

--- mainJobBatch/taskManage/serviceBase/mdScrapingLogicService.py
+++ mainJobBatch/taskManage/serviceBase/mdScrapingLogicService.py
@@ -1,13 +1,27 @@
 from abc import ABCMeta, abstractmethod
 
-##気象データ収集&ファイル作成ビジネスロジックインターフェース
 class MeteorologicaldataScrapingService(metaclass=ABCMeta):
+    """ 気象データ収集&ファイル作成ビジネスサービス基底インターフェース
+    """
+
     @abstractmethod
     def mainSoup(self):
+        """
+        Webスクレイピング及びファイル出力処理
+
+        Returns
+        ----------
+        end : str
+            ビジネスロジック処理終了サイン
+        """
+
         pass
 
     @abstractmethod
     def MDOutput(self):
+        """ 気象データ収集後のファイル作成処理
+        """
+
         pass
 
 
